eae793f802842f0d6cf88f987fadb0ca6209404e
wierza <dawid@wierzycki.pl>
2023-08-30 12:56:56 +0200

    Add files

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,18 @@
+import { convertPLNToUSD } from './../convertPLNToUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('-543')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+});
---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,15 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (PLN === undefined || typeof PLN === 'string') {
+    return NaN;
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+  
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
